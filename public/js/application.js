3db44ca12f2e1d87235fe444989f9a9cfb46bb1a
Apprentice <apprentice@dbc-chicago031.local>
2013-11-14 16:59:39 -0600

    modified js

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+
+  $(document).on("keydown", function(event){
+    keyCode = event.which;
+    // TAB = 9
+    // ENTER = 13
+    // console.log(keyCode)
+    if(keyCode == 9){
+      event.preventDefault();
+
+      currently_selected_rb = $('input[name=roll_type]:checked', '#roll_form');
+      debugger;
+      if (currently_selected_rb == $("input[name=roll_type]").last()){
+        $("input[name=roll_type]").first().prop("checked", true);
+      }
+      else {
+        currently_selected_rb.next().next().prop("checked", true);
+      }
+
+    }
+    else if (keyCode == 10){
+
+    }
+    else {
+      // do nothing FOR NOW
+    }
+  });
+
+
   $("#roll_form").on("submit", function(event){
     event.preventDefault();
 
