cb9f5e47dded6f7ddde7ee3f60bbbca0a1e3f688
Apprentice <apprentice@dbc-chicago031.local>
2013-11-15 12:16:51 -0600

    Removed excess comments

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,6 +6,9 @@ post '/roll' do
   dice_code = params[:dice_box]
   # parsed_dice_code = DiceCodeParser.parse(dice_code).flatten # REMOVE FLATTED WHEN REGEX UPDATED
 
+  # puts "CLIENT SENT:"
+  # p dice_code
+
   result = DicePool.new(dice_code).roll
 
   display_result(result)
