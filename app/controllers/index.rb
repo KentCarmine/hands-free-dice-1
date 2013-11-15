6552f0f637471fe4f1f3149b42672c860f5138f3
Apprentice <apprentice@dbc-chicago031.local>
2013-11-15 15:33:45 -0600

    added 10 again

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,15 +3,25 @@ get '/' do
 end
 
 post '/roll' do
+  # puts "ROLL TYPE #{params[:roll_type]}"
+  roll_type = params[:roll_type].strip
   dice_code = params[:dice_box]
+
+  # puts "ROLL TYPE: #{roll_type}"
   # parsed_dice_code = DiceCodeParser.parse(dice_code).flatten # REMOVE FLATTED WHEN REGEX UPDATED
 
   # puts "CLIENT SENT:"
   # p dice_code
 
-  result = DicePool.roll(dice_code)
+  result = DicePool.roll(dice_code, roll_type)
 
-  display_result(result)
+  if roll_type == "standard"
+    display_standard_result(result)
+  elsif roll_type == "10_again"
+    display_10_again_result(result)
+  else
+    "NOT VALID ROLL MODE"
+  end
 end
 
 get '/results_frame' do
--- app/models/DicePool.rb
+++ app/models/DicePool.rb
@@ -5,11 +5,16 @@ class DicePool
   end
 
   def self.roll(dice_code, ruleset="standard")
+    # puts "RULESET: "
+    # puts ruleset
     if ruleset == "standard"
+      # puts "USING STANDARD RULSET"
       self.standard_roll(dice_code)
-    elsif ruleset == "10-again"
-    elsif ruleset == "9-again"
-    elsif ruleset == "8-again"
+    elsif ruleset == "10_again"
+      # puts "USING 10-AGAIN RULSET"
+      self.ten_again_roll(dice_code)
+    elsif ruleset == "9_again"
+    elsif ruleset == "8_again"
     else
 
     end
@@ -45,6 +50,48 @@ class DicePool
     [roll_result, each_roll.join(", "), @operation, @constant]
   end
 
+  def self.ten_again_roll(dice_code)
+    num_dice = dice_code.to_i
+
+    roll_result = 0
+    all_rolls = []
+
+    @dice_collection = self.created_d_tens(num_dice)
+
+    @dice_collection.each do |dice|
+      all_rolls << self.roll_single_dice_again_on(10, dice)
+    end
+
+    all_rolls.flatten!.sort!
+    num_ones = self.count_ones(all_rolls)
+    num_successes = self.count_successes(all_rolls, 8)
+
+    [num_successes, num_ones, all_rolls.join(", ")]
+  end
+
+  def self.roll_single_dice_again_on(reroll_num, dice)
+    results = []
+
+    while true
+      roll = dice.roll()
+      results << roll
+
+      if roll < reroll_num
+        break
+      end
+    end
+
+    results
+  end
+
+  def self.count_successes(rolls, threshold = 8)
+    rolls.count {|roll| roll >= threshold}
+  end
+
+  def self.count_ones(rolls)
+    rolls.count {|roll| roll <= 1}
+  end
+
   def self.create_dice(parsed_code)
     num_dice = generate_num_dice(parsed_code)
     dice_size = parsed_code[1].to_i
@@ -55,6 +102,16 @@ class DicePool
 
     num_dice.times do
       dice_collection << Dice.new(:num_sides => dice_size)
+    end
+
+    dice_collection
+  end
+
+  def self.created_d_tens(num_d_tens)
+    dice_collection = []
+
+    num_d_tens.times do
+      dice_collection << Dice.new(:num_sides => 10)
     end
 
     dice_collection
